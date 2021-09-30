f62ee7dd7132320944949b7048d4b87fc8595b95
Steven Sarria <ssarr012@fiu.edu>
2021-09-30 18:23:19 -0400

    began working on homepage and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Header from './components/header'
+import HomePage from './pages/HomePage';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Route exact path='/' render={(props) => <HomePage {...props} />} />
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div class='bg-red-200'>
+            <div>
+                <h1 class='text-indigo-900 text-6xl'>Steven</h1>
+            </div>
+            <div>
+                <h1 class='text-indigo-900 text-6xl'>Sarria</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home
