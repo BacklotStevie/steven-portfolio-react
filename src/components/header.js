199a80483aa8447c0aacddabbd8b7b4d73537e93
Steven Sarria <ssarr012@fiu.edu>
2021-10-01 14:14:18 -0400

    fixed header spacing and homepage spacing

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,13 @@ const Header = ({ siteTitle }) => {
     return (
       <>
         <nav class="justify-between flex-wrap bg-red-200 p-5">
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-lg text-white text-center">
-            SS
-          </div>
+          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto justify-between">
+            <div class="text-lg text-indigo-900 pl-5 text-center">
+              SS
+            </div>
+            <div class="text-lg float-right text-indigo-900 pr-5 text-center">
+              About
+            </div>
         </div>
       </nav>
       </>
--- src/components/home.js
+++ src/components/home.js
@@ -4,11 +4,11 @@ import React from 'react';
 const Home = () => {
     return (
         <div class='bg-red-200'>
-            <div>
-                <h1 class='text-indigo-900 text-6xl'>Steven</h1>
+            <div class=''>
+                <h1 class='text-indigo-900 text-9xl pr-72 mr-96'>Steven</h1>
             </div>
-            <div>
-                <h1 class='text-indigo-900 text-6xl'>Sarria</h1>
+            <div class='flex justify-around'>
+                <h1 class='text-indigo-900 text-9xl pl-72 ml-96'>Sarria</h1>
             </div>
         </div>
     );
