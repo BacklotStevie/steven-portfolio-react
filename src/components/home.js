05fa14315400e1f2d77e6d9c40ffe2c9447b3539
Steven Sarria <ssarr012@fiu.edu>
2021-10-08 15:55:30 -0400

    changed styling for header

--- src/components/home.js
+++ src/components/home.js
@@ -3,14 +3,23 @@ import React from 'react';
 
 const Home = () => {
     return (
+    <div>
         <div class='bg-red-200'>
             <div class=''>
-                <h1 class='text-indigo-900 text-9xl pr-72 mr-96'>Steven</h1>
+                <h1 class='text-indigo-900 text-9xl pr-60 mr-50'>Steven</h1>
             </div>
-            <div class='flex justify-around'>
-                <h1 class='text-indigo-900 text-9xl pl-72 ml-96'>Sarria</h1>
+            <div class=''>
+                <h1 class='text-indigo-900 text-9xl pl-60 ml-50'>Sarria</h1>
+            </div>
+            <div>
+                <h3 class='text-indigo-900 pt-10 italic'>Developing your visions. Creating your dreams.</h3>
             </div>
         </div>
+
+        <div class='bg-red-200'>
+
+        </div>
+    </div> 
     );
 };
 
