de5c5217fe8797df0a8aba2b4475be563966d53a
Steven Sarria <ssarr012@fiu.edu>
2021-10-11 19:29:05 -0400

    began creating copy for main page

--- src/components/main.js
+++ src/components/main.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div>
-            <h1 class='text-indigo-900'>test</h1>
+        <div class='bg-red-200 h-screen'>
+            <div class='flex-col float-left ml-80 mt-36'>
+                <div>
+                    <h1 class='text-indigo-900 text-6xl italic tracking-tight'>Your Brand's</h1>
+                </div>
+                <div>
+                    <h1 class='text-indigo-900 text-6xl italic tracking-tight'>Best Friend</h1>
+                </div>
+            </div>
+
+            <div class='flex float-right mt-56 mr-64 w-half'>
+                <span class='text-indigo-900 text-base'>I enjoy creating things of significance to people's lives. My goal is bring dreams to life, whether it be through design, code, or other media.</span>
+            </div>
         </div>
     )
 }
