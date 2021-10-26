5d0da97792e1d54c995e84d62fa7acf8b6737eb4
Steven Sarria <ssarr012@fiu.edu>
2021-10-26 15:21:36 -0400

    added to porfolio projects, began creating about section.

--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import backlot from '../images/backlot.png'
 import trigan from '../images/trigan.png'
 import teramana from '../images/teramana.png'
+import fdot from '../images/fdot.png'
 
 const Main = () => {
     return (
@@ -24,7 +25,7 @@ const Main = () => {
             </div>
         </div>
 
-        <div class='bg-red-200 h-screen'>
+        <div class='bg-red-200'>
             <div class='pt-25'>
                 <h1 class='text-indigo-900 text-6xl italic tracking-tight'>Featured Work</h1>
             </div>
@@ -32,10 +33,10 @@ const Main = () => {
             <div class="bg-red-200 py-6 flex flex-col justify-center mx-auto sm:py-12">
                 <div class="h-auto py-10 px-10 w-2/3 bg-indigo-900 space-y-5 mx-auto rounded-xl shadow-xl hover:rotate-1 transition-transform">
                     <div>
-                        <h1 class='text-center text-xl pb-10 text-white underline'><a href="http://insidebacklot.com">Inside the Backlot</a></h1>
+                        <h1 class='text-center text-xl pb-10 text-red-200 underline'><a href="http://insidebacklot.com">Inside the Backlot</a></h1>
                     </div>
                     <div class='flex justify-between grid-cols-2'>
-                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-white lg:w-2/5">Inside the Backlot a film podcasting, blogging and social networking community. Created by myself and developed alongside Ariel Elazarov, Inside the Backlot is a full-stack web application created with React.js, Node.js and Bootstrap, with fully functional backend routing.</h2>
+                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-red-200 lg:w-2/5">Inside the Backlot a film podcasting, blogging and social networking community. Created by myself and developed alongside Ariel Elazarov, Inside the Backlot is a full-stack web application created with React.js, Node.js and Bootstrap, with fully functional backend routing.</h2>
                         <img src={backlot} alt="..." class="cols-span-12 max-h-80 w-1/2 border-none mt-3 pl-5"></img>
                     </div>
                 </div>
@@ -44,10 +45,10 @@ const Main = () => {
             <div class="bg-red-200 py-6 flex flex-col justify-center mx-auto sm:py-12">
                 <div class="h-auto py-10 px-10 w-2/3 bg-indigo-900 space-y-5 mx-auto rounded-xl shadow-xl hover:rotate-1 transition-transform">
                     <div>
-                        <h1 class='text-center text-xl pb-10 text-white underline'><a href='http://https://triganvisuals.netlify.app/'>TRI-GAN Visuals</a></h1>
+                        <h1 class='text-center text-xl pb-10 text-red-200 underline'><a href='http://https://triganvisuals.netlify.app/'>TRI-GAN Visuals</a></h1>
                     </div>
                     <div class='flex justify-between grid-cols-2'>
-                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-white lg:w-2/5">TRI-Gan Videos is a Miami-based videographer, editor and creative consultant. Their portfolio site was created using React.js, Gatsby.js and TailwindCSS.</h2>
+                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-red-200 lg:w-2/5">TRI-Gan Videos is a Miami-based videographer, editor and creative consultant. Their portfolio site was created using React.js, Gatsby.js and TailwindCSS.</h2>
                         <img src={trigan} alt="..." class="cols-span-12 max-h-80 w-1/2 border-none mt-3 pl-5"></img>
                     </div>
                 </div>
@@ -56,14 +57,33 @@ const Main = () => {
             <div class="bg-red-200 py-6 flex flex-col justify-center mx-auto sm:py-12">
                 <div class="h-auto py-10 px-10 w-2/3 bg-indigo-900 space-y-5 mx-auto rounded-xl shadow-xl hover:rotate-1 transition-transform">
                     <div>
-                        <h1 class='text-center text-xl pb-10 text-white underline'><a href='https://toningwithteremanatequila.netlify.app/'>Toning With Teramana Tequila</a></h1>
+                        <h1 class='text-center text-xl pb-10 text-red-200 underline'><a href='https://toningwithteremanatequila.netlify.app/'>Toning With Teramana Tequila</a></h1>
                     </div>
                     <div class='flex justify-between grid-cols-2'>
-                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-white lg:w-2/5">One of my projects while at Ironhack, Toning With Teramana Tequila was a group React.js project inspired by Dwayne 'The Rock' Johnson's new brand of tequila, Teramana Tequila. This project was done in conjecture with a UX group, giving us real-world experience in working with a UX/UI team.</h2>
+                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-red-200 lg:w-2/5">One of my projects while at Ironhack, Toning With Teramana Tequila was a group React.js project inspired by Dwayne 'The Rock' Johnson's new brand of tequila, Teramana Tequila. This project was done in conjecture with a UX group, giving us real-world experience in working with a UX/UI team.</h2>
                         <img src={teramana} alt="..." class="cols-span-12 max-h-80 w-1/2 border-none mt-3 pl-5"></img>
                     </div>
                 </div>
             </div>
+
+            <div class="bg-red-200 py-6 flex flex-col justify-center mx-auto sm:py-12">
+                <div class="h-auto py-10 px-10 w-2/3 bg-indigo-900 space-y-5 mx-auto rounded-xl shadow-xl hover:rotate-1 transition-transform">
+                    <div>
+                        <h1 class='text-center text-xl pb-10 text-red-200 underline'><a href='https://docs.google.com/document/d/1Rtal_F7Oi2Ht1sH9xraZwRsr3YHbg0CJ/edit?usp=sharing&ouid=111097312435884888160&rtpof=true&sd=true'>Don't Let Your Distraction Drive You - FDOT</a></h1>
+                    </div>
+                    <div class='flex justify-between grid-cols-2'>
+                        <h2 class="cols-span-12 font-normal tracking-wide text-xl text-red-200 lg:w-2/5">'Don't Let Your Distraction Drive You' is an advertisement and marketing campaing developed by a group of students for the Florida Department of Transportation. Our goal was to reach our target audience (college students) with a relevant and consice marketing campaign to instill better driving practices within the community.</h2>
+                        <img src={fdot} alt="..." class="cols-span-12 max-h-80 w-1/2 border-none mt-3 pl-5"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class='bg-indigo-900 '>
+            <div>
+                <h1 class='font-normal tracking-wide text-2xl text-red-200 lg:w-2/5 pt-64'>A Branding Consultant</h1>
+                <h2 class='font-normal tracking-wide text-2xl text-red-200 pt-20'>I am a web design, creative</h2>
+            </div>    
         </div>
     </>
     )
